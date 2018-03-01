0b82ed39a4b72a86d702f06d2c8f84bdf388ab0a
Nitin Rangarajan <nitinr92@gmail.com>
2018-02-28 23:47:05 -0500

    Added code to explain logic of for loops in R

--- Week1_StartingWithR/Week1_Conditional_Loops_Functions.R
+++ Week1_StartingWithR/Week1_Conditional_Loops_Functions.R
@@ -25,3 +25,24 @@ if(sum(x) > 500)
 # Loops in R
 for(i in 1:10) print(i)
 print("Hello")
+
+a <- rnorm(10)
+a.pos <- rep(NA, length(a)) #This is to simply declare and initialize nothing for size 1000
+a.pos
+# Code to copy only positive values from a to a.pos
+for(j in 1:length(a))
+{
+  if(a[j]<0)
+  {
+    a.pos[j] <- 0
+  }
+  else
+  {
+    a.pos[j]<- a[j]
+  }
+}
+a
+a.pos
+hist(a)
+hist(a.pos)
+
