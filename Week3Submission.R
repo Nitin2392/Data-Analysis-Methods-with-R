74f61805cbb57750cef680cf51599069ca04b081
Nitin Rangarajan <nitinr92@gmail.com>
2018-03-26 20:48:05 -0400

    Week 3 - Regression Analysis

---
+++ Week3Submission.R
@@ -0,0 +1,11 @@
+# Question 1
+getwd()
+setwd("C:/Users/Nitin Rangarajan/Desktop/UC Material/Courses/Spring/Flex2/Data Analysis Methods/Homeworks/Week3")
+getwd()
+tomb_data <- read.csv("tombstone.csv")
+head(tomb_data)
+
+# Question 2
+
+
+
